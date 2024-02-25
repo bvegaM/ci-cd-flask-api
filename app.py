31c63c8ee6579fa1c998e27949f694982540f5c1
bvegam <vegabryam40@gmail.com>
2024-02-25 16:36:57 -0500

    add database connection

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flaskuser:Bs1JdlZ0ZvRgWwRsvOikSQrUBKa80LlU@dpg-cndqlbun7f5s73bmod7g-a.oregon-postgres.render.com/dbsum'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Sum(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    num1 = db.Column(db.Integer, nullable=False)
+    num2 = db.Column(db.Integer, nullable=False)
+    result = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Sum {self.id}: {self.num1} + {self.num2} = {self.result}>'
+
 
 @app.route('/sum', methods=['POST'])
 def sum():
@@ -8,5 +25,16 @@ def sum():
     num1 = data['num1']
     num2 = data['num2']
     result = num1 + num2
+
+    # Guardar el resultado en la tabla Sum
+    sum_entry = Sum(num1=num1, num2=num2, result=result)
+    db.session.add(sum_entry)
+    db.session.commit()
+
     return jsonify({'result': result})
+
+with app.app_context():
+        db.drop_all()
+        db.create_all()
+
 
