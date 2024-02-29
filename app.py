f5062a9cb53af4b2ea2e702215b822381016d6de
bvegam <vegabryam40@gmail.com>
2024-02-28 19:36:10 -0500

    Add get all method

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from marshmallow import fields
 
 
 
@@ -10,7 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flaskuser:Bs1JdlZ0ZvRgWwRs
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-ma = Marshmallow(app)
 
 class Sum(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,22 +18,20 @@ class Sum(db.Model):
     def __repr__(self):
         return f'<Sum {self.id}: {self.num1} + {self.num2} = {self.result}>'
 
-
-# Crear el esquema SumSchema
-class SumSchema(ma.Schema):
-    id = fields.Integer()
-    num1 = fields.Integer()
-    num2 = fields.Integer()
-    result = fields.Integer()
-
-sums_schema = SumSchema(many=True)  
-
+    def serialize(self):
+        return {
+            'id': self.id,
+            'num1': self.num1,
+            'num2': self.num2,
+            'result': self.result
+        }
 
 
 @app.route('/sum', methods=['GET'])
 def find_all():
-    sums= db.session.execute(db.select(Sum)).scalars()
-    return sums_schema.jsonify(sums), 200
+    sums_scalar = db.session.execute(db.select(Sum)).scalars()
+    sums = [i.serialize() for i in sums_scalar]
+    return jsonify(sums), 200
 
 @app.route('/sum', methods=['POST'])
 def sum():
@@ -52,8 +47,11 @@ def sum():
 
     return jsonify({'result': result})
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+    app.run(debug=True)
 
 
